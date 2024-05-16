f82339299b2e48f1f145ec1dab26ab4a90090b7c
Neil Fritz <ncfritz@ncfritz.net>
2024-05-16 15:03:17 -0700

    First pass at incorporating semantic-release

---
+++ packages/model/release.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["/dist/**/*.js", "/dist/**/*.d.ts"],
+      },
+    ],
+  ],
+};
