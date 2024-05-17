373398052a1612248d7f862d6b5946780c0aaa69
Neil Fritz <ncfritz@ncfritz.net>
2024-05-16 21:42:43 -0700

    Build: One more attempt at getting versioning working

--- packages/model/release.config.js
+++ packages/model/release.config.js
@@ -27,18 +27,23 @@ module.exports = {
         changelogFile: "./CHANGELOG.md",
       },
     ],
-    "@semantic-release/release-notes-generator",
     [
-      "@semantic-release/github",
+      "@semantic-release/release-notes-generator",
       {
-        assets: ["README.md", "CHANGELOG.md", "dist", "package.json"],
+        preset: "ember",
       },
     ],
     [
-      "@semantic-release/git",
+      "@semantic-release/npm",
       {
-        message:
-          "Chore: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        npmPublish: false,
+        tarballDir: "dist",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/*.tgz"],
       },
     ],
   ],
