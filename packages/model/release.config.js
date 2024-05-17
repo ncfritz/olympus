bdec773ed898a975928c6cbd34c97444b9d876ad
Neil Fritz <ncfritz@ncfritz.net>
2024-05-16 17:44:35 -0700

    Build: Sets up semantic-release based on commit tags

--- packages/model/release.config.js
+++ packages/model/release.config.js
@@ -4,7 +4,23 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "ember",
+        releaseRules: [
+          { type: "Feature", scope: "README", release: "patch" },
+          { type: "Update", release: "patch" },
+          { type: "New", release: "minor" },
+          { type: "Breaking", release: "minor" },
+          { type: "New", release: "minor" },
+          { type: "Build", release: "patch" },
+          { type: "Upgrade", release: "minor" },
+          { type: "Chore", release: "patch" },
+          { scope: "no-release", release: false },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/github",
