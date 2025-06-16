5986c20cdc82e2457632de5afd3892d503d9d0dd
Neil Fritz <ncfritz@ncfritz.net>
2025-06-16 16:14:20 -0700

    Feature: Converters for notifications

---
+++ apps/api/src/convert/notifications/NotificationGroupConverter.ts
@@ -0,0 +1,33 @@
+import { NotificationGroup } from "@ncfritz/olympus-model/dist/notifications";
+import moment from "moment/moment";
+import { GraphQlFullNotificationType } from "./NotificationTypeConverter";
+import { toFullDomainObject } from "./NotificationTypeConverter";
+
+export type GraphQlNotificationGroup = {
+  id: string;
+  name: string;
+  description: string;
+  createdTime: string;
+  notificationTypes?: GraphQlFullNotificationType[];
+};
+
+export const toDomainObject = (
+  input: GraphQlNotificationGroup,
+): NotificationGroup => {
+  const notificationGroup: NotificationGroup = {
+    id: input.id,
+    name: input.name,
+    description: input.description,
+    createdTime: moment(input.createdTime),
+  };
+
+  if (input.notificationTypes) {
+    notificationGroup.notificationTypes = input.notificationTypes.map(
+      (entry) => {
+        return toFullDomainObject(entry);
+      },
+    );
+  }
+
+  return notificationGroup;
+};
