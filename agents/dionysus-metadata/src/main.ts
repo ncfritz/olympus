c9369c51eeb4e272de78da15151645f23fb66d02
Neil Fritz <ncfritz@ncfritz.net>
2025-06-22 14:59:01 -0700

    Fix: Set default listen port and plumb in LISTEN_PORT environment variable where necessary

--- agents/dionysus-metadata/src/main.ts
+++ agents/dionysus-metadata/src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
 
-  await app.listen(process.env.LISTEN_PORT || 3101);
+  await app.listen(process.env.LISTEN_PORT || 3100);
 }
 
 bootstrap()
