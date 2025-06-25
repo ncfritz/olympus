be1c07b333b9dc5daeb51e3b524d18a3ad918656
Neil Fritz <ncfritz@ncfritz.net>
2025-06-24 21:25:40 -0700

    Fix: Set /metrics content-type header to text/plain for compatibility with Prometheus scraping.

--- agents/dionysus-metadata/src/main.ts
+++ agents/dionysus-metadata/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory, PartialGraphHost } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import fs from "fs";
 import { WinstonModule } from "nest-winston";
+import { PrometheusMetricsInterceptor } from "./middleware/PrometheusMetricsInterceptor";
 import { AppModule } from "./module/AppModule";
 import { logger } from "./util/logger";
 import "dotenv/config";
@@ -24,6 +25,7 @@ async function bootstrap() {
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
+  app.useGlobalInterceptors(new PrometheusMetricsInterceptor());
 
   await app.listen(process.env.LISTEN_PORT || 3100);
 }
