eb8921df1fd63f4464e220d419313829eccda840
Neil Fritz <ncfritz@ncfritz.net>
2026-05-20 16:54:33 -0700

    Chore: Update CORS origins

--- agents/dionysus-asset/src/main.ts
+++ agents/dionysus-asset/src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors({
-    origin: ["http://localhost:3000", "https://dionysus.dev.ncfritz.net"],
+    origin: [
+      "http://localhost:3000",
+      "https://olympus.dev.ncfritz.net",
+      "https://olympus.internal.ncfritz.net",
+    ],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
