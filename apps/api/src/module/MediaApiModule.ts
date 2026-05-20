9ffd85ed86f68431b662b76addd6f34b6ae17a0e
Neil Fritz <ncfritz@ncfritz.net>
2026-05-20 16:24:41 -0700

    Feature: Adds controllers for media transcode workflows

--- apps/api/src/module/MediaApiModule.ts
+++ apps/api/src/module/MediaApiModule.ts
@@ -1,21 +1,31 @@
 import { Module } from "@nestjs/common";
+import { ApproveMediaAssetTranscodeConfigurationController } from "../controller/dionysus/media/ApproveMediaAssetTranscodeConfigurationController";
 import { BulkUpdateMediaAssetDownloadsController } from "../controller/dionysus/media/BulkUpdateMediaAssetDownloads";
 import { CreateMediaAssetController } from "../controller/dionysus/media/CreateMediaAsset";
 import { CreateMediaAssetDownloadController } from "../controller/dionysus/media/CreateMediaAssetDownload";
 import { CreateMediaAssetSearchConfigurationController } from "../controller/dionysus/media/CreateMediaAssetSearchConfiguration";
 import { CreateMediaAssetSearchExecutionController } from "../controller/dionysus/media/CreateMediaAssetSearchExecution";
 import { CreateMediaAssetSearchResultController } from "../controller/dionysus/media/CreateMediaAssetSearchResult";
+import { CreateMediaAssetWorkflowController } from "../controller/dionysus/media/CreateMediaAssetWorkflow";
+import { CreateMediaAssetWorkflowStepController } from "../controller/dionysus/media/CreateMediaAssetWorkflowStep";
+import { CreateMediaAssetWorkflowSubStepController } from "../controller/dionysus/media/CreateMediaAssetWorkflowSubStep";
 import { DescribeMediaAssetSearchConfigurationController } from "../controller/dionysus/media/DescribeMediaAssetSearchConfiguration";
 import { DescribeMediaAssetSearchExecutionController } from "../controller/dionysus/media/DescribeMediaAssetSearchExecution";
 import { DescribeMediaAssetSearchResultController } from "../controller/dionysus/media/DescribeMediaAssetSearchResult";
+import { DescribeMediaAssetWorkflowController } from "../controller/dionysus/media/DescribeMediaAssetWorkflow";
+import { DescribeMediaAssetWorkflowStepController } from "../controller/dionysus/media/DescribeMediaAssetWorkflowStep";
 import { GetMediaAssetSearchConfigurationsRunningCountController } from "../controller/dionysus/media/GetMediaAssetSearchConfigurationsRunningCount";
-import { ListMediaSearchExecutionsController } from "../controller/dionysus/media/ListMediaAssetSearchExecutions";
+import { ListMediaAssetSearchExecutionsController } from "../controller/dionysus/media/ListMediaAssetSearchExecutions";
 import { ListMediaAssetSearchResultsController } from "../controller/dionysus/media/ListMediaAssetSearchResults";
+import { ListMediaAssetWorkflowsController } from "../controller/dionysus/media/ListMediaAssetWorkflows";
 import { TriggerMediaAssetSearchController } from "../controller/dionysus/media/TriggerMediaAssetSearch";
 import { UpdateMediaAssetDownloadController } from "../controller/dionysus/media/UpdateMediaAssetDownload";
 import { UpdateMediaAssetDownloadByNzbIdController } from "../controller/dionysus/media/UpdateMediaAssetDownloadByNzbId";
 import { UpdateMediaAssetSearchConfigurationController } from "../controller/dionysus/media/UpdateMediaAssetSearchConfiguration";
 import { UpdateMediaAssetSearchExecutionController } from "../controller/dionysus/media/UpdateMediaAssetSearchExecution";
+import { UpdateMediaAssetWorkflowStepController } from "../controller/dionysus/media/UpdateMediaAssetWorkflowStep";
+import { VerifyMediaAssetTranscodeConfigurationController } from "../controller/dionysus/media/VerifyMediaAssetTranscodeConfigurationController";
+import { GetTvEpisodeByIdController } from "../controller/dionysus/metadata/tv/GetTvEpisodeById";
 
 import { GraphQLClientModule } from "./GraphQLClientModule";
 import { RabbitModule } from "./RabbitModule";
@@ -25,23 +35,33 @@ import { RabbitModule } from "./RabbitModule";
   exports: [],
   providers: [],
   controllers: [
+    ApproveMediaAssetTranscodeConfigurationController,
     BulkUpdateMediaAssetDownloadsController,
     CreateMediaAssetController,
     CreateMediaAssetSearchConfigurationController,
     CreateMediaAssetSearchExecutionController,
     CreateMediaAssetSearchResultController,
     CreateMediaAssetDownloadController,
+    CreateMediaAssetWorkflowController,
+    CreateMediaAssetWorkflowStepController,
+    CreateMediaAssetWorkflowSubStepController,
+    GetTvEpisodeByIdController,
     GetMediaAssetSearchConfigurationsRunningCountController,
     DescribeMediaAssetSearchConfigurationController,
     DescribeMediaAssetSearchExecutionController,
     DescribeMediaAssetSearchResultController,
-    ListMediaSearchExecutionsController,
+    DescribeMediaAssetWorkflowController,
+    DescribeMediaAssetWorkflowStepController,
+    ListMediaAssetSearchExecutionsController,
     ListMediaAssetSearchResultsController,
+    ListMediaAssetWorkflowsController,
     TriggerMediaAssetSearchController,
     UpdateMediaAssetDownloadController,
     UpdateMediaAssetDownloadByNzbIdController,
     UpdateMediaAssetSearchConfigurationController,
     UpdateMediaAssetSearchExecutionController,
+    UpdateMediaAssetWorkflowStepController,
+    VerifyMediaAssetTranscodeConfigurationController,
   ],
 })
 export class MediaApiModule {}
