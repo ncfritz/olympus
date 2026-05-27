512d9653e318fa0c6444fdb7f3655f9aa72dcd78
Neil Fritz <ncfritz@ncfritz.net>
2026-05-26 22:31:43 -0700

    Fix: Add initialization logging and signal handler

--- agents/dionysus-asset/src/main.ts
+++ agents/dionysus-asset/src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, PartialGraphHost } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import Ffmpeg from "fluent-ffmpeg";
+import fs from "fs";
+import moment from "moment";
 import { AppModule } from "./module/AppModule";
+import { logger } from "./util/logger";
+import { onExit } from "signal-exit";
+
 Ffmpeg.setFfmpegPath(process.env.FFMPEG_PATH!);
 Ffmpeg.setFfprobePath(process.env.FFPROBE_PATH!);
+
+const timestamp = moment.utc();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,4 +30,23 @@ async function bootstrap() {
   await app.listen(3102);
 }
 
-bootstrap();
+bootstrap()
+  .then(() => {
+    logger.info("Installing signal handler...");
+    onExit((code, signal) => {
+      logger.error(
+        `Exit handler triggered on signal ${signal} with code ${code}`,
+      );
+    });
+
+    logger.info("🔥🔥🔥 Olympus Asser Agent bootstrap complete.");
+  })
+  .catch((e) => {
+    logger.error("🤯🤯🤯 Error during bootstrap!", e);
+
+    fs.writeFileSync(
+      `/logs/${timestamp.unix()}-graph.json`,
+      PartialGraphHost.toString() ?? "",
+    );
+    process.exit(1);
+  });
