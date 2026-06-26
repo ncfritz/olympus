edd58c160ea4903a95661d573b16d9d38b25ac75
Neil Fritz <ncfritz@ncfritz.net>
2026-06-26 15:54:05 -0700

    Build: Migrate build to WebPack

---
+++ agents/olympus-notification/webpack.config.js
@@ -0,0 +1,64 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { RsdoctorWebpackPlugin } = require("@rsdoctor/webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const CopyPlugin = require("copy-webpack-plugin");
+
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+console.log("--- Webpack Build Initializing ---");
+console.log("Current Mode:", mode);
+
+module.exports = {
+  entry: "./src/main.ts",
+  mode: mode,
+  target: "node",
+  devtool: "source-map",
+  plugins: [
+    process.env.RSDOCTOR &&
+      new RsdoctorWebpackPlugin({
+        disableClientServer: false,
+        features: ["loader", "bundle", "plugins", "resolver", "treeShaking"],
+        output: {
+          mode: "normal",
+          options: {
+            type: ["html", "json"],
+            htmlOptions: {
+              writeDataJson: true,
+              reportHtmlName: "rsdoctor-report.html",
+            },
+            jsonOptions: {
+              reportJsonName: "rsdoctor-data.json",
+              writeDataJson: true,
+            },
+          },
+        },
+      }),
+    new CopyPlugin({
+      patterns: [{ from: "templates", to: "templates" }],
+    }),
+  ].filter(Boolean),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    chunkFormat: "commonjs",
+    library: {
+      type: "commonjs",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: "ts-loader" }],
+  },
+  optimization: {
+    splitChunks: false,
+    runtimeChunk: false,
+  },
+};
