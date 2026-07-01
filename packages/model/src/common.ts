ab90a5db4920fd0679f79e52a75598a60980db0b
Neil Fritz <ncfritz@ncfritz.net>
2026-06-30 18:49:19 -0700

    Feature: Allow sort to include nulls first/last

--- packages/model/src/common.ts
+++ packages/model/src/common.ts
@@ -2,7 +2,11 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export enum SortDirection {
   ASC = "asc",
+  ASC_NULL_FIRST = "asc_nulls_first",
+  ASC_NULL_LAST = "asc_nulls_last",
   DESC = "desc",
+  DESC_NULL_FIRST = "desc_nulls_first",
+  DESC_NULL_LAST = "desc_nulls_last",
 }
 
 export class EmptyResponse {}
