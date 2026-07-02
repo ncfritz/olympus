1f01263e1e1c8df6e142e131a56d6a26c2113a5a
Neil Fritz <ncfritz@ncfritz.net>
2026-07-02 13:49:48 -0700

    Fix: Propogate assetType and mediaId when creating a media asset download

--- apps/api/src/controller/dionysus/media/CreateMediaAssetDownload.ts
+++ apps/api/src/controller/dionysus/media/CreateMediaAssetDownload.ts
@@ -101,6 +101,8 @@ export class CreateMediaAssetDownloadController extends BaseMediaAssetSearchResu
             progress: $progress
             assetType: $assetType
             mediaId: $mediaId
+            assetType: $assetType
+            mediaId: $mediaId
           }
         ) {
           ${BASE_MEDIA_DOWNLOAD}
